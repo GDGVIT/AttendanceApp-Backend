6fb4812d2b423838b93819f4635e5ee1be625e30
root <kushchoudhary8@gmail.com>
2020-01-31 03:31:36 +0530

    minor fix: wrong file uploaded

--- app.py
+++ app.py
@@ -229,12 +229,33 @@ def user_signup():
     password = request.json['password']
     email=request.json['email']
 
-    new_user = Users(username, password, email)
+    email_check = Users.query.filter_by(email=email).first()
 
+    if email_check:
+        return jsonify({
+            'username':'',
+            'password':'',
+            'email':'',
+            'admin_status':0,
+            'jwt':'',
+            'status':'User Already Exsists'
+        })
+
+    new_user = Users(username, password, email)
     db.session.add(new_user)
     db.session.commit()
 
-    return user_schema.jsonify(new_user)
+    user = Users.query.filter_by(email=request.json['email']).first()
+    auth_token = encode_auth_token(user.id)
+
+    return jsonify({
+        'username':username,
+        'password':password,
+        'email':email,
+        'admin_status':0,
+        'jwt':auth_token.decode(),
+        'status':"User Created Successfully"
+    })
 
 
 # user login
@@ -255,8 +276,6 @@ def user_login():
                 'auth_token':auth_token.decode()
             })
     except Exception as e:
-        # print('\n\n\n', Users.query.filter_by(email=request.json['email']).first().password, '\n\n\n')
-        # print('\n\n\n', password, e , '\n\n\n')
         return jsonify({
             'status': 'fail',
             'message': 'Wrong Credentials! Check Again.'
@@ -431,12 +450,11 @@ def attendence_post():
 
 @socketio.on('join', namespace='/rooms_namespace')
 def on_join(data):
-    debug(str(data))
-    debug(str(type(data)))
-    data = json.loads(data)
-    #verify if attendence is success
-    #then get them join in that room name with otp added
-    #now broadcast to that room
+    """verify if attendence is success 
+    then get them join in that room name with otp added
+    now broadcast to that room
+    """
+    data = json.loads(data) # now it's dict form, before it was string
 
     status = data['status']
     if status!='Success':
@@ -455,6 +473,9 @@ def on_join(data):
 
 @socketio.on('admin_stats', namespace='/admin_namespace')
 def made_for_admin():
+    """To tell flask-SocketIO that there exsists a namespace named admin_namespace
+    then we can use it in functions written below. Or else error will be thrown.
+    """
     #admin_stats is mapped with admin_listen
     pass
 
@@ -480,6 +501,7 @@ def take_attendence_from_user(json_):
         datetime_ = datetime.datetime.now()
         # workleft: In client payLoad take latitude and longitude of user and then
         # compare it with admins lat and long and judge on distance param
+        # Make change after making change in events model
         status_ = 1 # code logic needed here
         try:
             event_id_ = Events.query.filter_by(otp=otp_).first().id
@@ -568,29 +590,19 @@ if __name__ == '__main__':
 
 
 
-## Do this:
-# when a new event is created, event-name (slug) is added in events db. Now when attende sends otp we take it and check if it matches with any \
-# event and event attendence is going on. Then we take attendence and emit this info to admin namespace.
-# also, attende attendence is sended to one more route that attendence is emited to that particular event room (if allowed for so by admin).
+""" To help someone who will work on this in future, To help the frontend person: (This may not be exactly True)
 
-# attendence is emitted to: 1) attendence namespace, from their attendence is done and emited to admin.
-# 2) broadcaster namespace, from there to specific room name if allowed by admin.
+when a new event is created, event-name (slug) is added in events db. Now when attende sends otp we take it and check if it matches with any
+event and event attendence is going on. Then we take attendence and emit this info to admin namespace.
+also, attende attendence is sended to one more route that attendence is emited to that particular event room (if allowed for so by admin).
 
-# admin_namespace -> to emit to the admin_namespace :emits to the client side namespace then  (SERVER SIDE NOT NEEDED)
-# attendence_namespace-> attendence (to emit client attendence) :emits to the client side admin_namespace then
-# broadcaster_namespace -> attendence comes to this namespace also, from here it's sended to particular room
+attendence is emitted to:
+1) attendence namespace, from their attendence is done and emited to admin.
+2) broadcaster namespace, from there to specific room name if allowed by admin.
 
-
-
-### SocketsIO how and what
-# User can only emit
-## the attendence is marked
-#event otp is required, and the token for that very user
-
-
-# Browser will Broadcast 1)attendences  (otp cann't be broadcasted)
-## When user enters the otp, it takes them to a dashbaord with that otp
-##and their we broadcast the attendences
+admin_namespace -> to emit to the admin_namespace :emits to the client side namespace then  (SERVER SIDE NOT NEEDED)
+attendence_namespace-> attendence (to emit client attendence) :emits to the client side admin_namespace then
+broadcaster_namespace -> attendence comes to this namespace also, from here it's sended to particular room """
 
 
 
@@ -601,10 +613,6 @@ if __name__ == '__main__':
 # Time check, before attendence verify it's in allowed time limit. Or frontend takes care of this and don't send vague requests \
 #or server takes request don't add to db and sends a message Attendence is closed now.
 #fetch creation_date and time_delta then before adding see if valid.
-
-### Implementing the socketIO
-# user emits a signal to us with otp then we process and commit it.
-
 # An API to give a few otp examples which are available
 
 
@@ -615,12 +623,21 @@ if __name__ == '__main__':
     # login, check
 # check for already registered users:-> not possible as we email is pk
 # make use of make_response, to add status code as well along with jsonify return
-# replace flask-jwt with flask-jwt-extended
+
 # from passlib.hash import pbkdf2_sha256 as sha256
-# eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MTE2Mzc2NDksImlhdCI6MTU4MDEwMTY0NCwic3ViIjoxfQ.QkM-DsJR0yuVbSL-ElkQeHG0xYPpF_BrA-lJhi8MZqk
-# clean dtetime.datetime.now()
+# clean datetime.datetime.now()
+
+
 # In Events table, admin name is used, can be either made unique during /
 #registration or make it admin email. Ask?
-# imporovements in model can be done, like create a function that takes id or email \
-#and gives all other details. then use this in all other functions
-# check if resp isinstance of str or not
+# add field admin_email in events model
+# add admin_email field in sockets attendence_namespace payLoad for admin
+
+
+## model changes
+# add field admin_email in events model
+
+## Not doing (left for future if someone works):
+# replace flask-jwt with flask-jwt-extended
+# empty password is allowed :) Yes, Auth is just to keep users track
+# only single ip connection allowed for 1 user
