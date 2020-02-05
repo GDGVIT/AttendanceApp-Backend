1a869fb2875885a5cfec9a75dc0f22e1260aa0d3
root <kushchoudhary8@gmail.com>
2020-02-05 19:23:17 +0530

    add:tests

--- app/tests/test_basic.py
+++ app/tests/test_basic.py
@@ -1,1 +1,19 @@
 import unittest
+from .. import *
+import unittest
+
+# pytest -v
+# python -m unitest
+
+
+class TestMyApp(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_404(self):
+        result = self.app.get('/other')
+        self.assertEqual(result.status, '404 NOT FOUND')
+
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
