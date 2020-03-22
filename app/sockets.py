2e99f48cb28eb892687ea13f6f755993313b567d
root <kushchoudhary8@gmail.com>
2020-03-22 19:20:58 +0530

    roll back

--- app/sockets.py
+++ app/sockets.py
@@ -240,13 +240,11 @@ def take_attendence_from_user(json_):
                     event_name_ = event.event_name
                     admin_email_ = event.admin_email
                     attendence_status = 'Success' # This param is for admin only
-                    broadcast_choice_ = str(event.broadcast_choice)
                 except:
                     event_name_ = ''
                     admin_email_ = ''
                     otp_ = -1
                     attendence_status = 'Failure' # This param is for admin only
-                    broadcast_choice_ = '0'
                 
                 payLoad = {
                     'email':user_info(token_).get("email"),
@@ -257,8 +255,7 @@ def take_attendence_from_user(json_):
                     'admin_email':admin_email_, # use this to send to specific admin
                     'otp':otp_,
                     'Reason':Reason,
-                    'StatusCode':StatusCode,
-                    'BroadcastChoice':broadcast_choice_,
+                    'StatusCode':StatusCode
                 }
                 emit('admin_listen', json.dumps(payLoad), namespace='/admin_namespace')
             else:
@@ -273,7 +270,6 @@ def take_attendence_from_user(json_):
             'admin_email':'',
             'otp':-1,
             'Reason':Reason,
-            'StatusCode':StatusCode,
-            'BroadcastChoice':broadcast_choice_,
+            'StatusCode':StatusCode
         }
         emit('admin_listen', json.dumps(payLoad), namespace='/admin_namespace')
